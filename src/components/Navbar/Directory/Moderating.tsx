91682ec7263223789b173d8e0add1f19db62ffc5
Shantanu Sameer <shantanu.sameer12@gmail.com>
2023-03-26 16:21:06 +0530

    1st commit

---
+++ src/components/Navbar/Directory/Moderating.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Box, Text } from "@chakra-ui/react";
+import { RiCommunityFill } from "react-icons/ri";
+import { CommunitySnippet } from "../../../atoms/communitiesAtom";
+import MenuListItem from "./MenuListItem";
+
+type ModeratingProps = {
+  snippets: CommunitySnippet[];
+};
+
+const Moderating: React.FC<ModeratingProps> = ({ snippets }) => {
+  return (
+    <Box mt={3} mb={3}>
+      <Text pl={3} mb={1} fontSize="7pt" fontWeight={500} color="gray.500">
+        MODERATING
+      </Text>
+      {snippets.map((snippet) => (
+        <MenuListItem
+          key={snippet.communityId}
+          displayText={`r/${snippet.communityId}`}
+          link={`r/${snippet.communityId}`}
+          icon={RiCommunityFill}
+          iconColor="brand.100"
+        />
+      ))}
+    </Box>
+  );
+};
+export default Moderating;
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Text, Flex, Image } from "@chakra-ui/react";
+import { User } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useSetRecoilState } from "recoil";
+import {
+  defaultMenuItem,
+  directoryMenuState,
+} from "../../atoms/directoryMenuAtom";
+import { auth } from "../../firebase/clientApp";
+import Directory from "./Directory";
+import RightContent from "./RightContent";
+import SearchInput from "./SearchInput";
+import router from "next/router";
+import useDirectory from "../../hooks/useDirectory";
+
+const Navbar: React.FC = () => {
+  const [user] = useAuthState(auth);
+
+  // Use <Link> for initial build; implement directory logic near end
+  const { onSelectMenuItem } = useDirectory();
+
+  return (
+    <Flex
+      bg="white"
+      height="44px"
+      padding="6px 12px"
+      justifyContent={{ md: "space-between" }}
+    >
+      <Flex
+        align="center"
+        width={{ base: "40px", md: "auto" }}
+        mr={{ base: 0, md: 2 }}
+        cursor="pointer"
+        onClick={() => onSelectMenuItem(defaultMenuItem)}
+      >
+        <Image src="/images/vasukamlogo.jpg" height="30px" />
+        <Text
+          display={{ base: "none", md: "unset" }}
+          fontSize="19px" 
+          padding="5px"
+          fontWeight="bold"
+        color='tomato'>
+            VASUKAM
+          </Text>
+      </Flex>
+      {user && <Directory />}
+      <SearchInput user={user as User} />
+      <RightContent user={user as User} />
+    </Flex>
+  );
+};
+export default Navbar;
